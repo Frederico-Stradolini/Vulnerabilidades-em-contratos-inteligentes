88659980c97a4f6ec01d546c63086214651cd2f2
Frederico-Stradolini <126301446+Frederico-Stradolini@users.noreply.github.com>
2025-01-21 18:32:15 -0300

    Update overflow_e_underflow.sol

--- overflow_e_underflow.sol
+++ overflow_e_underflow.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.7.0;
 // Versão anterior a ^0.8.0, onde não há proteção automática contra overflow e underflow
 
 contract ContratoNaoSeguro {
-    uint256 public totalSupply;
+    uint8 public totalSupply;
 
     constructor() {
         totalSupply = 255;
